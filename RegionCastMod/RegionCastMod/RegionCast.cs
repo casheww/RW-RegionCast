513565a7d20516963192ef5c6b46b65d0c6047d5
casheww <51793722+casheww@users.noreply.github.com>
2020-12-05 23:58:41 +0000

    overhaul mod part

---
+++ RegionCastMod/RegionCastMod/RegionCast.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using UnityEngine;
+
+namespace RegionCastMod
+{
+    [BepInPlugin("casheww.region_cast_discord", "RegionCast", "1.0.0")]
+    public class RegionCast : BaseUnityPlugin
+    {
+        System.Diagnostics.Process castRecApp = null;
+        DateTime lastUpdate = DateTime.Now;
+        Transmitter transmitter;
+
+        public RegionCast()
+        {
+            transmitter = new Transmitter(this);
+
+            On.RainWorld.Start += RainWorld_Start;
+            On.RainWorldGame.ExitToMenu += RainWorldGame_ExitToMenu;
+            On.Player.Update += Player_Update;
+        }
+
+        void RainWorld_Start(On.RainWorld.orig_Start orig, RainWorld self)
+        {
+            orig(self);
+
+            string path = Directory.GetCurrentDirectory() + @"\RegionCast-DiscordGameSDK\RegionCastApp.exe";
+            castRecApp = System.Diagnostics.Process.Start(path);
+
+            if (castRecApp is null)
+            {
+                Debug.LogError("RegionCast : RegionCastApp.exe was not found!\n" +
+                    "\tMake sure the DiscordGameSDK was placed in the Rain World root directory " +
+                    "i.e. next to the `Rain World.exe`. Always read the README ;)\n");
+                Destroy(GetComponent<RegionCast>());
+                return;
+            }
+
+            transmitter.SendUDP(Transmitter.GameMode.Menu);
+        }
+
+        void RainWorldGame_ExitToMenu(On.RainWorldGame.orig_ExitToMenu orig, RainWorldGame self)
+        {
+            orig(self);
+            transmitter.SendUDP(Transmitter.GameMode.Menu);
+        }
+
+        void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            orig(self, eu);
+
+            // 5 second cooldown - Player.Update is called every frame
+            DateTime currentTime = DateTime.Now;
+            if (currentTime.Subtract(lastUpdate) < TimeSpan.FromSeconds(5)) { return; }
+
+            string currentLocationName;
+            Transmitter.GameMode gameMode;
+
+            if (!(self.room.world.region is null))
+            {
+                // player is in a region (the region doesn't not exist...)
+                currentLocationName = self.room.world.region.name;
+
+                gameMode = Transmitter.GetGameMode(self.slugcatStats.name);
+            }
+            else
+            {
+                // player is not in a region. This probably means they're in arena/sandbox mode
+                currentLocationName = self.room.roomSettings.name;
+                gameMode = Transmitter.GameMode.Arena;
+            }
+
+            lastUpdate = currentTime;
+            transmitter.SendUDP(gameMode, currentLocationName);
+        }
+    }
+}
---
+++ RegionCastMod/RegionCastMod/Transmitter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+namespace RegionCastMod
+{
+    class Transmitter
+    {
+        static readonly Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint endpoint;
+
+        public Transmitter(RegionCast mod)
+        {
+            string configPath = Directory.GetCurrentDirectory() + @"\RegionCast-DiscordGameSDK\config.txt";
+            string[] config = File.ReadAllLines(configPath);
+
+            AttemptToMakeEndpoint(config, mod);
+        }
+
+        void AttemptToMakeEndpoint(string[] config, RegionCast mod)
+        {
+            int port;
+            try
+            {
+                port = Convert.ToInt32(config[0]);
+            }
+            catch (FormatException)
+            {
+                Debug.LogError("RegionCast : first line of RegionCast-DiscordGameSDK\\config.txt " +
+                    "(port number) could not be formatted to an int.");
+                UnityEngine.Object.Destroy(mod.GetComponent<RegionCast>());
+                return;
+            }
+
+            endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+        }
+
+        public enum GameMode
+        {
+            Menu,
+            Monk,
+            Survivor,
+            Hunter,
+            Arena
+            // todo: co-op compat?
+        }
+
+        public static GameMode GetGameMode(SlugcatStats.Name difficulty)
+        {
+            switch (difficulty)
+            {
+                case SlugcatStats.Name.Yellow:
+                    return GameMode.Monk;
+
+                default:
+                case SlugcatStats.Name.White:
+                    return GameMode.Survivor;
+
+                case SlugcatStats.Name.Red:
+                    return GameMode.Hunter;
+            }
+        }
+
+        public void SendUDP(GameMode gameMode, string location = "", int playerCount = 0)
+        {
+            /* Sends a UDP message to localhost:49181 where the RegionCastApp should be listening.
+             * gameMode : the current game mode 
+             * location : code of current region / name of current arena
+             * playerCount : may be used in future for extra compat features with Jolly Coop / Monkland */
+
+            string data = $"rwRegionCastData\n" +
+                $"gamemode:{gameMode}\n" +
+                $"location:{location}\n" +
+                $"playercount:{playerCount}";
+            byte[] message = Encoding.UTF8.GetBytes(data);
+            
+            socket.SendTo(message, endpoint);
+            Debug.Log($"RegionCast : UDP send to {endpoint.Address}:{endpoint.Port} for RegionCastApp");
+        }
+    }
+}
