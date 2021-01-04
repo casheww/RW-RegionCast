922a9e23af21de1a7b38bb3822d5cf0c72c29c20
casheww <51793722+casheww@users.noreply.github.com>
2021-01-04 17:05:01 +0000

    version bumps

--- mod/RegionCastMod/Properties/AssemblyInfo.cs
+++ mod/RegionCastMod/Properties/AssemblyInfo.cs
@@ -4,16 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("RegionCast")]
 [assembly: AssemblyDescription("A BepInEx plugin & side-app combo that adds Discord Rich Presence to Rain World.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("casheww")]
 [assembly: AssemblyProduct("RegionCast Mod")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyCopyright("Copyright © casheww 2020")]
 [assembly: ComVisible(false)]
-
 [assembly: Guid("2bd096c1-48e1-40ad-a8ce-bc1a5ae572b3")]
-
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
